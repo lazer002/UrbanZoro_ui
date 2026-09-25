2dc71933cb2a6d6cb759547c376a46ee7e90d628
lazer <ajlazer002@gmail.com>
2026-09-25 22:06:58 +0530

    lock

--- src/main.jsx
+++ src/main.jsx
@@ -13,19 +13,64 @@ import { AuthProvider } from "./state/AuthContext.jsx";
 import SmoothScroll from "./components/SmoothScroll.jsx";
 import ScrollManager from "./components/ScrollManager.jsx";
 
+const hasAccess = localStorage.getItem("garrib_access");
+
+const ComingSoon = () => (
+  <div
+    style={{
+      minHeight: "100vh",
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "center",
+      background: "#fff",
+      color: "#111",
+      fontFamily: "Arial, sans-serif",
+      textAlign: "center",
+      padding: "24px",
+    }}
+  >
+    <div>
+      <h1
+        style={{
+          fontSize: "48px",
+          fontWeight: "800",
+          margin: 0,
+          letterSpacing: "-2px",
+        }}
+      >
+        GARRIB
+      </h1>
+
+      <p
+        style={{
+          marginTop: "16px",
+          fontSize: "18px",
+          color: "#777",
+        }}
+      >
+        Coming Soon
+      </p>
+    </div>
+  </div>
+);
+
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <AuthProvider>
-        <BrowserRouter>
-          <WishlistProvider>
-            <SmoothScroll>
-              <ScrollManager />
-              <App />
-            </SmoothScroll>
-          </WishlistProvider>
-        </BrowserRouter>
-      </AuthProvider>
-    </Provider>
+    {hasAccess ? (
+      <Provider store={store}>
+        <AuthProvider>
+          <BrowserRouter>
+            <WishlistProvider>
+              <SmoothScroll>
+                <ScrollManager />
+                <App />
+              </SmoothScroll>
+            </WishlistProvider>
+          </BrowserRouter>
+        </AuthProvider>
+      </Provider>
+    ) : (
+      <ComingSoon />
+    )}
   </React.StrictMode>
 );
